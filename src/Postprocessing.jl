2971d724c7a6479d0b8efbd6db89a2d920c61d7e
Hendrik Ranocha <mail@ranocha.de>
2020-03-19 13:59:49 +0300

    group sparse total variation denoising

--- src/Postprocessing.jl
+++ src/Postprocessing.jl
@@ -1,10 +1,13 @@
 module Postprocessing
 
+using LinearAlgebra
+using OffsetArrays
 
 include("total_variation.jl")
 
 
-export total_variation_denoising, total_variation_denoising!
+export total_variation_denoising, total_variation_denoising!,
+       group_sparse_total_variation_denoising, group_sparse_total_variation_denoising!
 
 
 end # module
--- src/total_variation.jl
+++ src/total_variation.jl
@@ -22,7 +22,7 @@ In-place version of `total_variation_denoising(source, λ)` storing the result i
 """
 function total_variation_denoising!(dest::AbstractVector, source::AbstractVector, λ::Number)
   if λ < 0
-    throw(ArgumentError("The regularization parameter λ must be non-negative!"))
+    throw(ArgumentError("The regularization parameter λ must be non-negative: λ = $λ"))
   end
 
   idx = eachindex(dest, source)
@@ -105,6 +105,90 @@ function total_variation_denoising!(dest::AbstractVector, source::AbstractVector
       end
       break
     end
+  end
+
+  dest
+end
+
+
+
+"""
+    group_sparse_total_variation_denoising(y::AbstractVector, λ::Number, group_size::Integer=1)
+
+Compute the solution of the group sparse total variation regularized least square problem
+```math
+    \\min_x \\frac{1}{2} \\sum_{k} |y_k - x_k|^2 + \\lambda \\sum_{k} |x_{k+1} - x_{k}|
+```
+using the iterative algorithm of
+Selesnick and Chen (2013) Total variation denoising with overlapping group sparsity,
+doi: 10.1109/ICASSP.2013.6638755.
+"""
+function group_sparse_total_variation_denoising(source::AbstractVector, λ::Number;
+                                                group_size::Integer=1,
+                                                max_iter::Integer=100)
+  T = promote_type(eltype(source), typeof(λ))
+  dest = similar(source, T)
+  group_sparse_total_variation_denoising!(dest, source, convert(T, λ),
+                                          group_size=group_size, max_iter=max_iter)
+end
+
+function diff!(dx, x)
+  idx = eachindex(x)
+  @inbounds @simd for i in idx[1:end-1]
+    dx[i] = x[i+1] - x[i]
+  end
+  dx
+end
+
+function transpose_diff!(dx, x)
+  idx = eachindex(x)
+  @inbounds begin
+    dx[idx[1]] = -x[idx[1]]
+    @simd for i in idx[2:end-1]
+      dx[i] = x[i-1] - x[i]
+    end
+    dx[idx[end]] = x[idx[end]]
+  end
+  dx
+end
+
+function group_sparse_total_variation_denoising!(dest::AbstractVector, source::AbstractVector, λ::Number;
+                                                 group_size::Integer=1,
+                                                 max_iter::Integer=100)
+  if λ < 0
+    throw(ArgumentError("The regularization parameter λ must be non-negative: λ = $λ"))
+  end
+  if axes(dest) !== axes(source)
+    throw(DimensionMismatch("Dimensions must match: axes(dest) = $(axes(dest)) != $(axes(source)) = axes(source)"))
+  end
+
+  T = eltype(source)
+  dest .= source
+  b = similar(source, length(source)-1)
+  diff!(b, source)
+  tmp = similar(b)
+  u = OffsetArray(similar(source, length(source)-1+2*(group_size-1)), 2-group_size:length(source)+group_size-2)
+  u .= zero(T)
+  dl = fill(-one(T), length(source)-2)
+  F = Tridiagonal(dl, similar(source, length(source)-1), copy(dl))
+
+  for iter in Base.OneTo(max_iter)
+    diff!(u, dest)
+    @inbounds @simd for n in eachindex(F.d)
+      acc_outer = zero(T)
+      for j in 0:group_size-1
+        acc_inner = zero(T)
+        for k in 0:group_size-1
+          acc_inner += abs2(u[n - j + k])
+        end
+        acc_outer += inv(sqrt(acc_inner))
+      end
+      F.d[n] = 2 + inv(λ * acc_outer)
+    end
+    @. F.dl = F.du = -one(T)
+    ldiv!(tmp, lu!(F), b)
+    transpose_diff!(dest, tmp)
+    @. dest = source - dest
   end
 
   dest

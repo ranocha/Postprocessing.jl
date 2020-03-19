41b6eec214579692fdbd5178497ac6c5d5980b74
Hendrik Ranocha <mail@ranocha.de>
2020-03-19 13:59:49 +0300

    group sparse total variation denoising

--- test/total_variation_test.jl
+++ test/total_variation_test.jl
@@ -13,3 +13,15 @@ using LinearAlgebra, Random
     @test_throws DimensionMismatch total_variation_denoising!(similar(y_noisy, T, length(y_noisy)-1), y_noisy, 1)
   end
 end
+
+@testset "group sparse total variation denoising" begin
+  for T in (Float32, Float64)
+    x = range(zero(T), 2*one(T), length=1000)
+    y_true = sinpi.(x)
+    y_noisy = y_true .+ 0.1 .* randn(T, length(x))
+    y_denoised = group_sparse_total_variation_denoising(y_noisy, 1, group_size=3)
+    @test norm(y_denoised - y_true) < norm(y_noisy - y_true)
+    @test_throws ArgumentError group_sparse_total_variation_denoising(y_noisy, -1, group_size=3)
+    @test_throws DimensionMismatch group_sparse_total_variation_denoising!(similar(y_noisy, T, length(y_noisy)-1), y_noisy, 1)
+  end
+end
